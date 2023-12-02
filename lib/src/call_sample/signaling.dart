cce95ef7e1585cee6c9c969913d9c97ce0138b39
slamancn <gaoyanbo_31854@163.com>
2023-12-02 18:50:47 +0800

    android compile success

--- lib/src/call_sample/signaling.dart
+++ lib/src/call_sample/signaling.dart
@@ -459,13 +459,6 @@ class Signaling {
       */
     }
     pc.onIceCandidate = (candidate) async {
-      if (candidate == null) {
-        print('onIceCandidate: complete!');
-        return;
-      }
-      // This delay is needed to allow enough time to try an ICE candidate
-      // before skipping to the next one. 1 second is just an heuristic value
-      // and should be thoroughly tested in your own environment.
       await Future.delayed(
           const Duration(seconds: 1),
           () => _send('candidate', {
@@ -507,7 +500,7 @@ class Signaling {
   }
 
   Future<void> _createDataChannel(Session session,
-      {label: 'fileTransfer'}) async {
+      {label = 'fileTransfer'}) async {
     RTCDataChannelInit dataChannelDict = RTCDataChannelInit()
       ..maxRetransmits = 30;
     RTCDataChannel channel =
